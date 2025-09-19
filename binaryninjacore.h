6118455664978568182455156134174718038011
Glenn Smith <glenn@vector35.com>
2025-09-19 17:33:27 -0400

    Merge strategy enum

--- binaryninjacore.h
+++ binaryninjacore.h
@@ -4159,6 +4159,17 @@ extern "C"
 		char* stringPostfix;
 	} BNCustomStringTypeInfo;
 
+	typedef enum BNMergeStrategy
+	{
+		IgnoreMergeStrategy,
+		MergeChildrenMergeStrategy,
+		AutoResolveLeftMergeStrategy,
+		AutoResolveRightMergeStrategy,
+		UserResolveLeftMergeStrategy,
+		UserResolveRightMergeStrategy,
+		PresentConflictMergeStrategy
+	} BNMergeStrategy;
+
 	BINARYNINJACOREAPI char* BNAllocString(const char* contents);
 	BINARYNINJACOREAPI char* BNAllocStringWithLength(const char* contents, size_t len);
 	BINARYNINJACOREAPI void BNFreeString(char* str);
