b5b5624ce1c326bfbca9c5b4277ed9e9868eb451
Peter LaFosse <peter@vector35.com>
2025-10-15 13:07:53 -0400

    Add BNRunUnitTests API

--- binaryninjacore.h
+++ binaryninjacore.h
@@ -3815,6 +3815,10 @@ extern "C"
 	BINARYNINJACOREAPI void BNShutdown(void);
 	BINARYNINJACOREAPI bool BNIsShutdownRequested(void);
 
+#ifdef UNIT
+	BINARYNINJACOREAPI int BNRunUnitTests(int argc, char** argv);
+#endif
+
 	BINARYNINJACOREAPI BNVersionInfo BNGetVersionInfo(void);
 	BINARYNINJACOREAPI char* BNGetVersionString(void);
 	BINARYNINJACOREAPI uint32_t BNGetBuildId(void);
