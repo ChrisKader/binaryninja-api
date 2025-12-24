7211162555254736617902858681788049580112
Glenn Smith <glenn@vector35.com>
2025-12-23 23:28:31 -0500

    DBO: Keep ref of parent in api to prevent ez uaf

--- binaryninjaapi.h
+++ binaryninjaapi.h
@@ -23068,6 +23068,8 @@ namespace BinaryNinja {
 
 	class DatabaseObject: public CoreRefCountObject<BNDatabaseObject, BNNewDatabaseObjectReference, BNFreeDatabaseObject>
 	{
+		Ref<DatabaseObject> m_parent;
+
 	public:
 		explicit DatabaseObject(BNDatabaseObject* database);
 
